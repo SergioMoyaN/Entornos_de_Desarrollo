3eef73021eeab89c1d218a5fc97ef1c7b0f8c26e
Sergio Moya <info.sergiomoya@gmail.com>
2026-05-16 22:52:51 +0200

    Primer commit del proyecto

--- EntornosDesarrolloGIT/src/entornosdesarrollogit/EntornosDesarrolloGIT.java
+++ EntornosDesarrolloGIT/src/entornosdesarrollogit/EntornosDesarrolloGIT.java
@@ -4,7 +4,7 @@ package entornosdesarrollogit;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto modificado");;
+        System.out.println("ModificacionPrueba");;
     }
 }
 
